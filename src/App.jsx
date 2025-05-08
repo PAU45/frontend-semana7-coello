4e4a06eae5a9d1c1d93914c2e9624e5568a6cca6
PAU45 <paulo.melendez@tecsup.edu.pe>
2025-05-08 06:48:34 -0500

    hola5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import DashboardUser from "./pages/dashboardUser";
 import DashboardModerator from "./pages/dashboardMod";
 import DashboardAdmin from "./pages/dashboardAdmin";
