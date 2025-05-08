ec67dcb4ea004f81f1a55a0051caf0af7a4d093d
PAU45 <paulo.melendez@tecsup.edu.pe>
2025-05-08 07:04:00 -0500

    rutas2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // Asegúrate de importar Routes y Route
 import DashboardUser from "./pages/dashboardUser";
 import DashboardModerator from "./pages/dashboardMod";
 import DashboardAdmin from "./pages/dashboardAdmin";
@@ -10,15 +10,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard/" element={<DashboardUser />} />
-        <Route path="/dashboard/moderator" element={<DashboardModerator />} />
-        <Route path="/dashboard/admin" element={<DashboardAdmin />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard/" element={<DashboardUser />} />
+      <Route path="/dashboard/moderator" element={<DashboardModerator />} />
+      <Route path="/dashboard/admin" element={<DashboardAdmin />} />
+    </Routes>
   );
 };
 
